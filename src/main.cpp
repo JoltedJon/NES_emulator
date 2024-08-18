1bc74afbd9dcff57db0fe9509ece15f6e2c23d10
Jonathon Sonneborn <sonnejm@umich.edu>
2024-08-17 23:59:11 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "sdlpp.hpp"
+
+int main(int argc, char** argv) {
+  sdl::Init init(SDL_INIT_EVERYTHING);
+  sdl::Window w("NES Emulator", 50, 50, 500, 500, SDL_WINDOW_BORDERLESS);
+  SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+
+  bool done = false;
+
+  sdl::EventHandler e;
+  e.quit = [&done](const SDL_QuitEvent&) { done = true; };
+
+  while (!done) {
+    while (e.poll()) {
+    }
+  }
+
+  return 0;
+}
